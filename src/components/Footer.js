a1351e88af256efbea801cad1188ea3dab64f91f
imramesh222 <imrameshrawat@gmail.com>
2024-06-26 17:08:42 +0545

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <div class="container">
-  <footer class="py-5">
+  <div class="px-4 bg-secondary">
+  <footer class="pt-5">
     <div class="row">
       <div class="col-6 col-md-2 mb-3">
         <h5>Section</h5>
@@ -20,19 +20,18 @@ const Footer = () => {
       <div class="col-6 col-md-2 mb-3">
         <h5>Section</h5>
         <ul class="nav flex-column">
-        <a href="./product-details.html" class="btn btn-primary"><i class="bi bi-eye"></i>View
-        Details</a>
+        
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Products</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
         </ul>
       </div>
 
-      <div class="col-6 col-md-2 mb-3">
+      <div class="col-6 col-md-2 mb-3 ">
         <h5>Section</h5>
-        <ul class="nav flex-column">
+        <ul class="nav flex-column ">
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
           <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
